1329d219e8fd53b4a460fc0e2408bc8d909b65e9
Mae-Berces <eaajmmb@students.eaaa.dk>
2021-02-25 10:39:19 +0100

    scrolltop
    scrolltop

--- wp-astra-child/script.js
+++ wp-astra-child/script.js
@@ -4,14 +4,28 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("The DOM is ready!");
     
 });
-//--------- smooth scroll -----------//
-document.querySelector("body").innerHTML += /*html*/
-<a id="scroolTop" onclick="scrollToTheTop()" title="Go to top">^</a>
-;
+// ---------- smooth scroll ---------- //
 
-window.onscroll = function () {
-    scrollFunction();
+function initScrollToTop() {
+    document.querySelector("body").innerHTML += /*html*/`
+        <a id="scrollTop" onclick="scrollToTheTop()" title="Go to top">^</a>
+    `;
+    window.onscroll = function () {
+        scrollFunction();
+    };
 }
-function scrollFunction(){
-    if (document.body.scrollTop > 500 
+
+function scrollFunction() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        document.getElementById("scrollTop").style.display = "block";
+    } else {
+        document.getElementById("scrollTop").style.display = "none";
+    }
+}
+
+function scrollToTheTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
 }
